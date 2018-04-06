bfb0e48a02b0d9a5658be8599be3272f982db355
Saurav Gyawali <saurav.gyawali@yahoo.com>
2018-04-06 18:31:57 -0400

    Changed fire,py

--- Navigation/fire.py
+++ Navigation/fire.py
@@ -9,3 +9,17 @@ GPIO.setup(24, GPIO.IN)
 
 def any_on():
 	return GPIO.input(14) or GPIO.input(15) or GPIO.input(18) or GPIO.input(23) or GPIO.input(24)
+
+def angle():
+	angle = []
+	if GPIO.input(14):
+		angle.append(20)
+	if GPIO.input(15):
+		angle.append(40)
+	if GPIO.input(18):
+		angle.append(60)
+	if GPIO.input(23):
+		angle.append(80)
+	if GPIO.input(24):
+		angle.append(100)
+	return sum(angle)/len(angle)
