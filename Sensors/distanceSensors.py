e9f229d30d65c25b3487b6d3dc58615b9e2737ae
Saurav Gyawali <saurav.gyawali@yahoo.com>
2018-04-07 01:53:24 -0400

    Changed distanceSensor.py

--- Sensors/distanceSensors.py
+++ Sensors/distanceSensors.py
@@ -1,45 +1,59 @@
 import RPi.GPIO as GPIO
 import time
+import numpy as np
 
 #ULTRASONICS_PINS = [[4, 20]]; #TRIGGER then ECHO
-ULTRASONICS_PINS = [[4, 20], [4, 16], [4, 21], [4, 5],
-						[4, 6], [4, 13], [4, 19], [4, 19]]; #TRIGGER then ECHO
+ULTRASONICS_PINS = [20,16, 21, 5, 13, 26, 6, 19]; #TRIGGER then ECHO
 TIMEOUT = 6/343
 TRIG = 4
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-for PINS in ULTRASONICS_PINS:
-	GPIO.setup(PINS[0], GPIO.OUT)
-	GPIO.setup(PINS[1], GPIO.IN)
+for Echo in ULTRASONICS_PINS:
+	GPIO.setup(Echo, GPIO.IN)
+GPIO.setup(TRIG, GPIO.OUT)
 
 def getDistances():
 	distances = []
 
-	# while isEcho():               #Check whether the ECHO is LOW
-	# 	a = 1
-	pulse_start = time.time() #Saves the last known time of LOW pulse
-
-	GPIO.output(TRIG, True)                  #Set TRIG as HIGH
-	time.sleep(0.00001)                      #Delay of 0.00001 seconds
-	GPIO.output(TRIG, False)                 #Set TRIG as LOW
+	init_time = time.time()
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
 	
-	pulse_stop = [pulse_start] * len(ULTRASONICS_PINS)
+	pulse_start = [time.time()] * len(ULTRASONICS_PINS)
+	pulse_stop = [time.time()] * len(ULTRASONICS_PINS)
 	seen_pulse = [False] * len(ULTRASONICS_PINS)
-	# (not (all(seen_pulse) and not isEcho()) or (
-	while (time.time() < (pulse_start+TIMEOUT)):
-		for index, pin_combo in enumerate(ULTRASONICS_PINS):
-			if GPIO.input(pin_combo[1]) == 1:
-				print time.time()
-				pulse_stop[index] = time.time()
-				seen_pulse[index] = True
+	stopped_pulse = [False] * len(ULTRASONICS_PINS)
 
+
+	while (time.time() < init_time + TIMEOUT):
+		for index, pin_combo in enumerate(ULTRASONICS_PINS):
+			if GPIO.input(pin_combo) == 1:
+				if not seen_pulse [index]:
+					pulse_start[index] = time.time()
+					seen_pulse[index] = True
+			if seen_pulse[index] and GPIO.input(pin_combo) == 0 and not stopped_pulse[index]:
+				pulse_stop[index] = time.time()
+				stopped_pulse[index] = True;
 	distances = []
 
-	for val in pulse_stop:
-		distances.append((val - pulse_start) * 17150)
+	for index, val in enumerate(pulse_stop):
+		distances.append((val - pulse_start[index]) * 17150)
 
 	return distances
+
+def getMedianDistances(num):
+	lists = []
+	a = []
+	for i in range(0,num):
+		distances = getDistances()
+		lists.append(distances)
+	lists = np.array(lists)
+	lists = lists.transpose()
+	for i in lists:
+		a.append(np.median(i))
+	return a
 
 def isEcho():
 	for pin in ULTRASONICS_PINS:
@@ -47,33 +61,6 @@ def isEcho():
 			return True
 	return False
 
-def _getDistance(pin_combination):
-	TRIG = pin_combination[0]
-	ECHO = pin_combination[1]
-
-	GPIO.output(TRIG, False)                 #Set TRIG as LOW
-	print "Waitng For Sensor To Settle"
-	time.sleep(2)                            #Delay of 2 seconds
-
-
-
-	while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
-		pulse_start = time.time()              #Saves the last known time of LOW pulse
-
-	while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
-		pulse_end = time.time()                #Saves the last known time of HIGH pulse 
-
-	pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
-
-	distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
-	distance = round(distance, 2)            #Round to two decimal points
-
-	if True:      #Check whether the distance is within range
-		print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
-	else:
-		print"Out Of Range"                   #display out of range
-
-	return distance
 
 def __del__():
 	for PINS in ULTRASONICS_PINS:
@@ -81,12 +68,12 @@ def __del__():
 			GPIO.cleanup(PIN)
 
 
-
 if __name__ == '__main__':
 	try:
+		lists = [] 
 		while True:
-			getDistances()
-			time.sleep(1)
+			dist = getMedianDistances(5)
+			print (dist[7])
 			print('-----------------------')
 	except KeyboardInterrupt:
 		__del__()
