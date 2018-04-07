19dead53595fbb9c616d1db1d9d21cd5b4c9b244
Saurav Gyawali <saurav.gyawali@yahoo.com>
2018-04-06 22:48:29 -0400

    Changed distanceSensor.py'

--- Sensors/distanceSensors.py
+++ Sensors/distanceSensors.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-ULTRASONICS_PINS = [[4, 20]]; #TRIGGER then ECHO
-# ULTRASONICS_PINS = [[4, 20], [4, 16], [4, 21], [4, 5],
-# 						[4, 6], [4, 13], [4, 19], [4, 19]]; #TRIGGER then ECHO
-TIMEOUT = 3/343
+#ULTRASONICS_PINS = [[4, 20]]; #TRIGGER then ECHO
+ULTRASONICS_PINS = [[4, 20], [4, 16], [4, 21], [4, 5],
+ 						[4, 6], [4, 13], [4, 19], [4, 19]]; #TRIGGER then ECHO
+TIMEOUT = 6/343
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -15,10 +15,30 @@ for PINS in ULTRASONICS_PINS:
 
 def getDistances():
 	distances = []
-	for pin_combination in ULTRASONICS_PINS:
-		_getDistance(pin_combination)
 
-	return distances
+	while isEcho():               #Check whether the ECHO is LOW
+		a = 1              #Saves the last known time of LOW pulse
+
+	GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+	time.sleep(0.00001)                      #Delay of 0.00001 seconds
+	GPIO.output(TRIG, False)                 #Set TRIG as LOW
+	
+	pulse_start = time.time()
+	pulse_stop = [pulse_start] * len(ULTRASONICS_PINS)
+
+
+	while isEcho() and time.time() < pulse_start+TIMEOUT:
+		for index, pin_combo in enumerate(ULTRASONICS_PINS):
+			if GPIO.input(pin_combo[1]) == 1
+				pulse_stop[index] = time.time()
+
+	return pulse_stop
+
+def isEcho():
+	for pin in ULTRASONICS_PINS:
+		if GPIO.input(pin[1]) == True:
+			return True
+	return False
 
 def _getDistance(pin_combination):
 	TRIG = pin_combination[0]
@@ -28,9 +48,7 @@ def _getDistance(pin_combination):
 	print "Waitng For Sensor To Settle"
 	time.sleep(2)                            #Delay of 2 seconds
 
-	GPIO.output(TRIG, True)                  #Set TRIG as HIGH
-	time.sleep(0.00001)                      #Delay of 0.00001 seconds
-	GPIO.output(TRIG, False)                 #Set TRIG as LOW
+
 
 	while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
 		pulse_start = time.time()              #Saves the last known time of LOW pulse
