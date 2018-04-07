503a191403490a8eb9b216ccd3ea967a3f745498
Ahmed Gado <ahmedehabg@gmail.com>
2018-04-07 04:51:12 -0400

    Main BANG

--- FinalScripts/extinguishFire.py
+++ FinalScripts/extinguishFire.py
@@ -1,10 +1,15 @@
 import extinguisher
 from time import sleep
 
+LIGHT_PIN = 8
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LIGHT_PIN, GPIO.OUT)
 def extinguishFire():
 	extinguisher.start()
 	sleep(.5)
 	extinguisher.stop()
 
 if __name__ == '__main__':
+	GPIO.output(LIGHT_PIN, 1)
 	extinguishFire()
+	GPIO.output(LIGHT_PIN, 0)
---
+++ FinalScripts/main.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python3 waitSound.py
+
+python3 blinkBlue.py &
+python3 traverseMaze.py
+
+python3 extinguishFire.py
