029c1762bf805e4d32164f8c16f002c038941e0d
Ahmed Gado <ahmedehabg@gmail.com>
2018-04-07 04:26:40 -0400

    add more thresholds

--- Navigation/wall_following.py
+++ Navigation/wall_following.py
@@ -2,7 +2,8 @@ import robot
 import distanceSensors
 import time
 
-TARGET_DISTANCE = 10 #cm
+FRONT_DISTANCE = 10 #cm
+RIGHT_DISTANCE = 5  #cm
 
 def wall_following_mode():
 	while True:
@@ -10,14 +11,14 @@ def wall_following_mode():
 		front_distance = dist(2)
 		right_distance = dist(0)
 
-		front_on = front_distance > TARGET_DISTANCE
-		right_on = right_distance < TARGET_DISTANCE
+		front_on = front_distance > FRONT_DISTANCE
+		right_on = right_distance < RIGHT_DISTANCE
 
 		if (not front_on and not right_on):
 			robot.rotate(-50)
 		elif (front_on and not right_on):
 			robot.rotate(50)
-		elif (front_off and right_on):
+		elif (not front_on and right_on):
 			robot.moveWheels(50, 50)
 		elif (front_on and right_on):
 			robot.rotate(50)
